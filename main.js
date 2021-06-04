dcd270f1a7a6c44117f6130a1ad97157e0c0881a
Greg Sronce <greg.sronce@gmail.com>
2021-06-04 12:26:03 -0400

    Cleaned up a few things.

--- main.js
+++ main.js
@@ -2,34 +2,26 @@ const form = document.getElementById("input-field");
 const head = document.getElementById("header");
 const artistSearch = document.getElementById("artist-search");
 const artistDisplay = document.querySelector(".song-info-container");
-const searchTitle = document.createElement("p");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (artistSearch.value === "") {
-    event.preventDefault();
-  } else {
-    form.remove();
-    const newSearch = document.createElement("button");
-    newSearch.classList.add("clear-search-button");
-    newSearch.innerText = "New Search";
-    head.appendChild(newSearch);
-    newSearch.addEventListener("click", (event) => {
-      if (event.target) {
-        location.reload();
-      }
-    });
-    const line = document.createElement("hr");
-    head.appendChild(line);
+  form.remove();
 
-    searchTitle.classList.add("search-results");
-    searchTitle.innerText = "Search Results:";
-    getInfo();
-  }
+  const newSearch = document.createElement("button");
+  newSearch.classList.add("clear-search-button");
+  newSearch.innerText = "New Search";
+  head.appendChild(newSearch);
+  newSearch.addEventListener("click", (event) => {
+    location.reload();
+  });
+  
+  const line = document.createElement("hr");
+  head.appendChild(line);
+
+  getInfo();
 });
 
 function getInfo() {
-//   json = {};
   const url = `https://proxy-itunes-api.glitch.me/search?term=${artistSearch.value}&limit=18&media=music&entity=song`;
   fetch(url)
     .then((res) => res.json())
@@ -48,7 +40,6 @@ function getInfo() {
         song.classList.add("song-title");
         const songTitle = document.createElement("a");
         songTitle.classList.add("link");
-        songTitle.href = "#";
         songTitle.innerText = result.trackName;
         song.appendChild(songTitle);
         artistBox.appendChild(song);
@@ -58,35 +49,31 @@ function getInfo() {
         artistBox.appendChild(artistName);
 
         songTitle.addEventListener("click", (event) => {
-          if (event.target.classList.contains("link")) {
-            const figure = document.createElement("figure");
-            figure.classList.add("figure");
-            head.appendChild(figure);
+          const figure = document.createElement("figure");
+          figure.classList.add("figure");
+          head.appendChild(figure);
 
-            const songPreview = document.createElement("audio");
-            songPreview.classList.add("audio-player");
-            songPreview.setAttribute("type", "audio/mpeg");
-            songPreview.setAttribute("controls", "controls");
-            songPreview.src = result.previewUrl;
-            figure.appendChild(songPreview);
+          const songPreview = document.createElement("audio");
+          songPreview.classList.add("audio-player");
+          songPreview.setAttribute("type", "audio/mpeg");
+          songPreview.setAttribute("controls", "controls");
+          songPreview.src = result.previewUrl;
+          figure.appendChild(songPreview);
 
-            const figCaption = document.createElement("figcaption");
-            figCaption.classList.add("fig-caption");
-            figCaption.innerText = "Now playing: " + result.trackName;
-            figure.appendChild(figCaption);
+          const figCaption = document.createElement("figcaption");
+          figCaption.classList.add("fig-caption");
+          figCaption.innerText = "Now playing: " + result.trackName;
+          figure.appendChild(figCaption);
 
-            const resetButton = document.createElement("button");
-            resetButton.classList.add("remove-song-button");
-            resetButton.innerText = "Remove Song";
-            figure.appendChild(resetButton);
-            resetButton.addEventListener("click", (event) => {
-              if (event.target) {
-                figure.remove();
-              }
-            });
-            const line = document.createElement("hr");
-            figure.appendChild(line);
-          }
+          const resetButton = document.createElement("button");
+          resetButton.classList.add("remove-song-button");
+          resetButton.innerText = "Remove Song";
+          figure.appendChild(resetButton);
+          resetButton.addEventListener("click", (event) => {
+            figure.remove();
+          });
+          const line = document.createElement("hr");
+          figure.appendChild(line);
         });
       }
     });
